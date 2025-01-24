13b6d8ac7d5b1dbfd1cf496f6a5bdb99bb7b4379
kshitizPratap <kshitiz100mit@gmail.com>
2025-01-24 19:36:14 +0530

    basic CRUD apis

--- app.js
+++ app.js
@@ -1,38 +1,77 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blogs");
 
 const app = express();
+const dbURI =
+  "mongodb+srv://node-tut:wU9Lr3NkF4mUj6@cluster0.asikm.mongodb.net/Tutorial?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(dbURI).then(() => {
+  app.listen(3000);
+});
+
 app.set("view engine", "ejs");
 
-app.listen(3000);
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "Yoshi finds eggs",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Mario finds stars",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "How to defeat bowser",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-  ];
+  res.redirect("/blogs");
+});
 
-  res.render("index", {
-    title: "Home",
-    blogs,
+app.get("/blogs", (req, res) => {
+  Blog.find().then((result) => {
+    res.render("index", {
+      title: "Home",
+      blogs: result,
+    });
   });
 });
 
-app.get("/about", (req, res) => {
-  res.render("about", { title: "About" });
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+  blog
+    .save()
+    .then(() => {
+      res.redirect("/blogs");
+    })
+    .catch(() => {
+      res.send("Error while posting the blog");
+    });
 });
 
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create" });
+});
+
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+
+  Blog.findById(id)
+    .then((response) => {
+      res.render("detailBlog", { title: "Blog", blog: response });
+    })
+    .catch(() => {
+      res.send("Error while fetching the blog");
+    });
+});
+
+app.delete("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then(() => {
+      res.json({
+        redirect: "/blogs",
+      });
+    })
+    .catch(() => {
+      res.send("Error while deleting the post");
+    });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
 });
 
 app.use((req, res) => {
